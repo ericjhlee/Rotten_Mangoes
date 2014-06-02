bdd3afee04381256c5f274bbe353dcf00e5b2bda
Eric Lee <eric_lee93@hotmail.com>
2014-06-02 05:49:09 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RottenMangoes::Application.routes.draw do
+  resources :movies
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +55,3 @@ RottenMangoes::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
